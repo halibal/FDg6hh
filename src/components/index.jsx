7c4a84095299d0afd6ed6941b4a9fcf6ff492b4e
Halibal <halibal95@gmail.com>
2023-04-22 21:47:54 +0300

    base folder structure created; images,color palette added; packages installed

---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export { default as Auth } from "./login";
+export { default as Category } from "./category";
+export { default as Header } from "./header";
+export { default as Product } from "./product";
+export { default as Slider } from "./slider";
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+// information in bookStoreInfo created using ChatGPT4
+
+export const bookStoreInfo = {
+    name: "LitRealm", // This name combines the words "Literature" and "Realm," suggesting a vast and magical world of books that awaits readers.
+    phone: "+44 (0) 20 1234 5678",
+    title: "LitRealm",
+    email: "info@bookstore-placeholder.com",
+    desc: "Welcome to LitRealm, your ultimate online destination for discovering and purchasing an incredible range of books across all genres. We are dedicated to igniting your passion for reading by offering a vast selection of titles, from bestsellers and hidden gems to timeless classics and new releases. Our user-friendly platform makes it easy to browse, search, and find your next literary adventure, while our competitive prices and quick shipping ensure a seamless shopping experience. Join the LitRealm community today and let us be your guide through the enchanting world of literature.",
+    about: "Welcome to LitRealm, a vibrant online hub for literature enthusiasts and lifelong learners. Founded in [year], our mission is to connect readers worldwide with the stories and ideas that inspire, challenge, and entertain. We believe that books have the power to transform lives, spark conversation, and open minds to new perspectives.At LitRealm, we pride ourselves on offering an extensive and diverse selection of titles, catering to readers of all ages, interests, and backgrounds. Our meticulously curated collection spans a multitude of genres, including fiction, non-fiction, children's books, self-help, biographies, and more. We are dedicated to staying ahead of the curve, regularly updating our inventory with the latest releases, undiscovered gems, and time-honored classics. Our user-friendly platform has been designed with your convenience in mind. Easy navigation, personalized recommendations, and an efficient search function enable you to discover your next great read with just a few clicks. And with our competitive pricing and fast shipping, LitRealm ensures a seamless and enjoyable shopping experience. We are more than just a bookstore; we are a community of passionate readers, eager to share our love for the written word. Connect with us on social media, and join our newsletter for exclusive promotions, author interviews, and literary news. Let LitRealm be your go-to source for enriching your literary journey.Thank you for choosing LitRealm - your portal to a world of boundless imagination and knowledge.",
+};
+
+export const settings = {
+    baseUrl: "https://assign-api.piton.com.tr/api/rest"
+}
