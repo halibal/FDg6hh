00604ea2a8888efda472173b4a2b56c377707aa4
Halibal <halibal95@gmail.com>
2023-04-23 13:23:01 +0300

    book-card created; category and book-card components completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
-import { Slider } from "@/components";
+import { Category, Slider } from "@/components";
 
 export default function HomePage() {
-	return <main className="mt-[40px]">
+	return <main className="max-w-[1340px] mx-auto my-[40px] flex flex-col gap-[40px]">
 		<Slider />
+		{/* map over category coming from redux */}
+		<Category />
+		<Category />
+		<Category />
+		<Category />
 	</main>;
 }
--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { BookCard } from "..";
 
 export default function Category() {
-    return <div>Category</div>;
+    return (
+        <div className="flex flex-col gap-5">
+            <div className="flex justify-between">
+                <h2 className="text-custom-4 font-bold text-[32px] leading-[44px]">
+                    Category Name
+                </h2>
+                <button className="text-custom-2 bg-none font-bold text-xl px-5">
+                    View All
+                </button>
+            </div>
+            <div className="flex gap-5 flex-nowrap">
+                {/* map over category and get first 4 items */}
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+            </div>
+        </div>
+    );
 }
